34ca90739f673c6616d4dfce407a66a6564d207b
Ryan Kotsovolos <rkotsovolos@gmail.com>
2021-10-02 10:29:47 -0700

    updated nav bar and added a footer

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 const h2 = document.createElement("h2");
 h2.textContent = "Future Software Engineer";
 document.querySelector("body").appendChild(h2);
+
+const footer = document.createElement("footer");
+footer.textContent = "My Projects";
+document.querySelector("section").appendChild(footer);
