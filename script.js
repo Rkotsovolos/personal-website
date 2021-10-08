db41f9607503ee5324a5f5e673149d40721fb87c
Ryan Kotsovolos <rkotsovolos@gmail.com>
2021-10-08 12:02:45 -0700

    added mouse over colors to the links

--- script.js
+++ script.js
@@ -1,15 +1,5 @@
-/*const h2 = document.createElement("h2");
-h2.textContent = "Future Software Engineer";
-document.querySelector("#heading").appendChild(h2);
-*/
+document.getElementById('school').addEventListener('onmouseover', mouseOver);
 
-
-/*function addingEventListener() {
-    const li = document.getElementById('li');
-li.addEventListener('click', function() {
-  alert('I was clicked!');
-});
+function mouseOver () {
+  document.getElementById('school').style.color = 'pink';
 }
-*/
-
-onmouse
